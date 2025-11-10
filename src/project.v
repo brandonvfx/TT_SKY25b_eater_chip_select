39ea49403ad896552b3d23364799c0ec002863fd
Brandon Ashworth <brandon@brandonashworth.com>
2025-11-09 23:53:54 -0800

    First pass on porting verilog from wokwi verilog node

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_6502_chip_select (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,10 +16,39 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  reg [7:0] data_out;
+  
+  always @ (posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      data_out <= 8'b0;
+    end
+  end
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+
+  wire cs_clk = ui_in[0];
+  wire A11 = ui_in[1];
+  wire A12 = ui_in[2];
+  wire A13 = ui_in[3];
+  wire A14 = ui_in[4];
+  wire A15 = ui_in[5];
+
+  wire peripheral_select = (~A15 & A14);
+  // put your logic here, e.g.:
+  always @ (posedge clk) begin
+    data_out[7] <= 0;
+    data_out[6] <= ~A15;
+    data_out[5] <= ~(~A15 & ~cs_clk);
+    data_out[4] <= A14;
+    data_out[3] <= ~peripheral_select;
+    data_out[2] <= peripheral_select & A13;
+    data_out[1] <= peripheral_select & A12;
+    data_out[0] <= ~(peripheral_select & ~A13 & ~A12 & A11);
+  end
+
+  assign uo_out = data_out;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
--- test/test.py
+++ test/test.py
@@ -25,16 +25,130 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
+    # Nothing On
+    
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # dut.ui_in.value = 20
+    dut.ui_in.value = 0b00000000
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 0b01001001
+    
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
+    await ClockCycles(dut.clk, 5)
+
+    # A14 on
+
+    dut.ui_in.value = 0b00100000
+
+    # Wait for two clock cycle to see the output values
+    await ClockCycles(dut.clk, 2)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+
+    assert dut.uo_out.value == 0b00101001
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+    await ClockCycles(dut.clk, 5)
+
+    # cs clock only
+
+    dut.ui_in.value = 0b00000001
+
+    # Wait for two clock cycle to see the output values
+    await ClockCycles(dut.clk, 2)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+
+    assert dut.uo_out.value == 0b01101001
+
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
+    await ClockCycles(dut.clk, 5)
+
+    # A14 & A15
+
+    dut.ui_in.value = 0b00110000
+
+    # Wait for two clock cycle to see the output values
+    await ClockCycles(dut.clk, 2)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+
+    assert dut.uo_out.value == 0b00111001
+    await ClockCycles(dut.clk, 5)
+
+    # A14 & cs_clk
+
+    dut.ui_in.value = 0b00010001
+
+    # Wait for two clock cycle to see the output values
+    await ClockCycles(dut.clk, 2)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+
+    assert dut.uo_out.value == 0b01110001
+    await ClockCycles(dut.clk, 5)
+
+    # A14 & ~15
+
+    dut.ui_in.value = 0b00010000
+
+    # Wait for two clock cycle to see the output values
+    await ClockCycles(dut.clk, 2)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+
+    assert dut.uo_out.value == 0b01010001
+    await ClockCycles(dut.clk, 5)
+
+
+    # A14 & A13
+
+    dut.ui_in.value = 0b00011000
+
+    # Wait for two clock cycle to see the output values
+    await ClockCycles(dut.clk, 2)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+
+    assert dut.uo_out.value == 0b01010101
+    await ClockCycles(dut.clk, 5)
+
+    # A14 & A12
+
+    dut.ui_in.value = 0b00010100
+
+    # Wait for two clock cycle to see the output values
+    await ClockCycles(dut.clk, 2)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+
+    assert dut.uo_out.value == 0b01010011
+    await ClockCycles(dut.clk, 5)
+
+    # A14 & A11
+
+    dut.ui_in.value = 0b00010010
+
+    # Wait for two clock cycle to see the output values
+    await ClockCycles(dut.clk, 2)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+
+    assert dut.uo_out.value == 0b01010000
+    await ClockCycles(dut.clk, 5)
